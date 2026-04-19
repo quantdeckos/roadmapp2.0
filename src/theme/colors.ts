09d1715a446579dd99b9944e5160507895f06f25
Gunner <quant.deckusa@gmail.com>
2026-04-19 13:09:55 -0400

    Add theme colors

---
+++ src/theme/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  neon: "#F3FF2D",
+  charcoal: "#16181E",
+  charcoalSoft: "#23262E",
+  panelDark: "#1D2028",
+  textPrimary: "#F5F7FA",
+  textMuted: "#9CA0AA",
+  lineMuted: "#3A3F4A",
+  success: "#7DFF77",
+  cyan: "#53F2DD"
+};
